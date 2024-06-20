7ea1cd1dbe1e03892f18c1905d70c8e0cfef05a6
Ilayprs <ilay.preiss10@gmail.com>
2024-06-20 20:17:34 +0300

    commit

--- HomePage/Server1.js
+++ HomePage/Server1.js
@@ -4,3 +4,26 @@ app.use(express.static('public'));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function send(username, password, selection){
+    return ("name: " + username + " pass: " + password + " type: " + selection);
+}
+app.post('/send', function (req, res) {
+    let name = req.body.username;
+    let pass = req.body.password;
+    let type = req.body.selection;
+
+    let result = send(name, pass, type);
+
+    res.end(`<html>
+                <body>
+                    <h2>${result}</h2>
+                </body>
+            </html>`);
+});
+
+app.get('/data', function(req, res) {
+    res.send(output);
+});
+
+app.listen(81);
