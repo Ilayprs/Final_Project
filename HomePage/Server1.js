514fc8e845afc90bae682f9e9e1183f554d45bd2
Ilayprs <ilay.preiss10@gmail.com>
2024-06-20 20:01:14 +0300

    commit

---
+++ HomePage/Server1.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
