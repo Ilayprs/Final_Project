2667c6264a09b55491170bb59ce5103fbd7bcaea
Ilayprs <ilay.preiss10@gmail.com>
2024-06-20 19:35:24 +0300

    commit

---
+++ HomePage/Sign_Up.js
@@ -0,0 +1,16 @@
+
+function passInfo(){
+    let type = document.getElementById('selection').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if(username==""){
+        alert("Enter your name");
+    }
+    if(password==""){
+        alert("Enter your password");
+    }
+    sessionStorage.setItem('username', username);
+    sessionStorage.setItem('type', type);
+    sessionStorage.setItem('password', password);
+    window.location.href = 'Sign_In.html';
+}
