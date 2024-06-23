972bce14dbd1cab8fb54c65f9e90a1d577fca40e
Ilayprs <ilay.preiss10@gmail.com>
2024-06-23 13:56:24 +0300

    Server1

--- Server1.js
+++ Server1.js
@@ -6,14 +6,42 @@ const PORT = 3000;
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'HomePage')));
 
-// Routes
+// Middleware to parse URL-encoded bodies and JSON bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Function to process login data
+function send(username, password, selection) {
+    return `name: ${username} pass: ${password} type: ${selection}`;
+}
+
+// Route to serve the Sign In page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'HomePage', 'Sign_In.html'));
 });
 
+// Route to serve the Sign Up page
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HomePage', 'Sign_Up.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HomePage', 'Log_In.html'));
+});
+
+// Route to handle form submission
+app.post('/send', (req, res) => {
+    const { username, password, selection } = req.body;
+    const result = send(username, password, selection);
+    res.send(`
+        <html>
+            <body>
+                <h2>${result}</h2>
+            </body>
+        </html>
+    `);
+});
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-//
