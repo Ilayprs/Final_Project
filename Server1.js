9a89ccaa32decaf161d3dfdd044e73e91899adac
Ilayprs <ilay.preiss10@gmail.com>
2024-06-24 16:33:36 +0300

    Server

--- Server1.js
+++ Server1.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose'); // Require Mongoose
-const { Int32 } = require('bson');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
@@ -14,7 +13,6 @@ app.use(express.json());
 mongoose.connect('mongodb+srv://IlayPrs:aYUsj9u46pprNGSU@project.kvttl0m.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -23,28 +21,19 @@ db.once('open', function() {
     console.log('Connected to MongoDB');
 });
 
-// Define a Mongoose schema and model for your data (example)
+// Define Mongoose schemas and models for Customer and Manager
 const customerSchema = new mongoose.Schema({
-    id: {type: Number, unique: true},
+    id: { type: Number, unique: true },
     username: String,
     password: String,
     city: String,
     credit: { type: Number, default: 0 },
 });
 
-customerSchema.statics.createCustomer = function(id, username, password, city) {
-    return new this({ id, username, password, city, credit: 0 });
-};
-
-customerSchema.methods.setCredit = function(credit) {
-    this.credit = credit;
-};
-
 const Customer = mongoose.model('Customer', customerSchema);
 
 const managerSchema = new mongoose.Schema({
-    id: {type: Number, unique: true},
-
+    id: { type: Number, unique: true },
     username: String,
     password: String,
     city: String,
@@ -52,54 +41,39 @@ const managerSchema = new mongoose.Schema({
 
 const Manager = mongoose.model('Manager', managerSchema);
 
-// Function to process login data (using Mongoose) in js bbbb
+// Function to create a new Customer
 async function sendCustomer(id, username, password, city) {
     const newUser = Customer.createCustomer(id, username, password, city);
     await newUser.save();
     return newUser;
 }
 
+// Function to create a new Manager
 async function sendManager(id, username, password, city) {
     const newUser = new Manager({ id, username, password, city });
     await newUser.save();
     return newUser;
 }
 
-const findManagerById = async (managerId) => {
+// Function to find a Manager by ID
+async function findManagerById(managerId) {
     try {
-        // Connect to the database
-
-        // Search for the manager by ID
         const manager = await Manager.findOne({ id: managerId });
-
-        // Check if the manager was found
-        if (manager) {
-            console.log('Manager found:');
-        } else {
-            console.log('Manager not found');
-        }
-    }catch (err) {
+        return manager;
+    } catch (err) {
         console.error('Error searching for manager:', err);
+        throw err;
     }
 }
 
-const findCustomerById = async (managerId) => {
+// Function to find a Customer by ID
+async function findCustomerById(customerId) {
     try {
-        // Connect to the database
-
-        // Search for the manager by ID
-        const manager = await Customer.findOne({ id: managerId });
-
-        // Check if the manager was found
-        if (manager) {
-            console.log('Customer found:');
-            
-        } else {
-            console.log('Customer not found');
-            
-        }
-    }catch (err) {
+        const customer = await Customer.findOne({ id: customerId });
+        return customer;
+    } catch (err) {
         console.error('Error searching for customer:', err);
+        throw err;
     }
 }
 
@@ -109,74 +83,89 @@ app.get('/', (req, res) => {
 });
 
 // Route to serve the Sign Up page
-+app.get('/signup', (req, res) => {
+app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, 'HomePage', 'Sign_Up.html'));
 });
 
+// Route to serve the Log In page
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'HomePage', 'Log_In.html'));
 });
 
+// Route to serve the Customer Store page
 app.get('/Cus_Store.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'store', 'Cus_Store.html'));
 });
 
+// Route to serve the Manager Store page
 app.get('/Man_Store.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'store', 'Man_Store.html'));
 });
 
-
-
-
+// Route to handle login form submission
 app.post('/send2', async (req, res) => {
-    const {id, selection} = req.body;
+    const { id, selection } = req.body;
 
-    if(selection === 'customer'){
-       findCustomerById(id);
-       res.send(`
-        <html>
-            <body onload="window.location.href='Cus_Store.html'">
-                
-            </body>
-        </html>
-    `);
-
-    } else { //is manager
-        findManagerById(id);
-        res.send(`
+    try {
+        if (selection === 'customer') {
+            const customer = await findCustomerById(id);
+            if (customer) {
+                res.send(`
+                    <html>
+                        <body onload="window.location.href='Cus_Store.html'">
+                            
+                        </body>
+                    </html>
+                `);
+            } else {
+                throw new Error('Customer not found');
+            }
+        } else { // is manager
+            const manager = await findManagerById(id);
+            if (manager) {
+                res.send(`
+                    <html>
+                        <body onload="window.location.href='Man_Store.html'">
+                            
+                        </body>
+                    </html>
+                `);
+            } else {
+                throw new Error('Manager not found');
+            }
+        }
+    } catch (err) {
+        console.error('Error processing login:', err);
+        res.status(400).send(`
             <html>
-                <body onload="window.location.href='Man_Store.html'">
-                    
+                <body>
+                    <script>
+                        alert('Incorrect');
+                        window.location.href = '/';
+                    </script>
                 </body>
             </html>
         `);
     }
-
 });
 
-// Route to handle form submission
+// Route to handle sign up form submission
 app.post('/send', async (req, res) => {
     const { id, username, password, selection, city } = req.body;
+
     try {
         if (selection === 'customer') {
             await sendCustomer(id, username, password, city);
-            res.send(`
-                <html>
-                    <body onload="window.location.href='Sign_In.html'">
-
-                    </body>
-                </html>
-            `);
-        } else { //is manager 
+        } else { // is manager
             await sendManager(id, username, password, city);
- +           res.send(`
-+                <html>
-                    <body onload="window.location.href='Sign_In.html'">
-                        
-                    </body>
-                </html>
-            `);
         }
+        res.send(`
+            <html>
+                <body onload="window.location.href='Sign_In.html'">
+                    
+                </body>
+            </html>
+        `);
     } catch (err) {
         console.error('Error saving data:', err);
         res.status(500).send('Error saving data');
