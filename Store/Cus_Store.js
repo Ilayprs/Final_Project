d75f55be5a1f543a5b86a26dbe330ca90807ca0a
Ilay Sasson <sassonelay83@gmail.com>
2024-07-07 12:20:13 +0300

    fj

---
+++ Store/Cus_Store.js
@@ -0,0 +1,9 @@
+/* 
+const id = localStorage.getItem('id');
+const userName = localStorage.getItem('userName');
+
+document.getElementById('userName').innerText = 'Name: ' + userName;
+
+document.getElementById('userId').innerText = 'ID: ' + id;
+document.getElementById('userType').innerText = 'Type: customer'
+*/
