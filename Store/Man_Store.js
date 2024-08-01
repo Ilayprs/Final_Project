b9796076e46e455709b36c1bba4b1f05ef7e43fc
Ilay Sasson <sassonelay83@gmail.com>
2024-08-01 14:39:37 +0300

    man js

--- Store/Man_Store.js
+++ Store/Man_Store.js
@@ -1,9 +1,6 @@
 // Fetch user details from localStorage
 const id = localStorage.getItem('id');
 const userName = localStorage.getItem('userName');
-document.getElementById('userName').innerText = 'Name: ' + userName;
-document.getElementById('userId').innerText = 'ID: ' + id;
-document.getElementById('userType').innerText = 'Type: manager';
 // Function to open a specific modal
 function openModal(modalId) {
     closeModal(); // Close any open modal first
